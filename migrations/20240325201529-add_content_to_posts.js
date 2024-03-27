b03b0aec327828bac4d289492f61d4a7d466688e
kristie fisher <fisherkristie19@icloud.com>
2024-03-27 12:49:49 -0400

    changed migration files

--- migrations/20240325201529-add_content_to_posts.js
+++ migrations/20240325201529-add_content_to_posts.js
@@ -2,45 +2,15 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('posts', {
-      id: {
-        type: Sequelize.DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      title: {
-        type: Sequelize.DataTypes.STRING,
-        allowNull: false
-      },
-      body: {
-        type: Sequelize.DataTypes.TEXT,
-        allowNull: false
-      },
-      user_id: {
-        type: Sequelize.DataTypes.INTEGER,
-        allowNull: true,
-        references: {
-          model: 'user',
-          key: 'id'
-        }
-      },
-      created_at: {
-        type: Sequelize.DataTypes.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-        field: 'created_at'
-      },
-      updated_at: {
-        type: Sequelize.DataTypes.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
-        field: 'updated_at'
-      }
+    await queryInterface.addColumn('post', 'body', {
+      type: Sequelize.TEXT,
+      allowNull: true,
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('posts');
+    await queryInterface.removeColumn('post', 'body');
   }
 };
+
 
--- migrations/20240325221325-change-title-column;.js
+++ migrations/20240325221325-change-title-column;.js
@@ -2,15 +2,17 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // Correct usage of DataTypes
-    await queryInterface.addColumn('posts', 'newColumn', {
-      type: Sequelize.DataTypes.STRING,
-      allowNull: true,
+    await queryInterface.changeColumn('post', 'title', {
+      type: Sequelize.STRING,
+      allowNull: true, // or false depending on your requirements
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('posts', 'newColumn');
+    await queryInterface.changeColumn('post', 'title', {
+      type: Sequelize.STRING,
+      allowNull: false, // Restore NOT NULL constraint if needed
+    });
   }
 };
 
