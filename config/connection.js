927082ed01b72731828aa3149d3458ae8db27b66
kristie fisher <fisherkristie19@icloud.com>
2024-03-28 17:55:43 -0400

    add config/connection to post-routes

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,20 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
+
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  // For deployment
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  // For local development
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
     dialect: 'mysql',
-    port: 3306,
-    logging: console.log, 
+    port: 3306
   });
 }
 
-// Check database connection status
-sequelize.authenticate()
-  .then(() => {
-    console.log('Database connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
 
 module.exports = sequelize;
-
 
