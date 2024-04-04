d3ff429257cc41219d6a61d7390b1d11bd20188c
kristie fisher <fisherkristie19@icloud.com>
2024-04-04 18:55:03 -0400

    troubleshoot deployment and connection.js file

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,18 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+  console.log('Using JAWSDB');
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  console.log('Using local database');
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
+    host: process.env.DB_HOST, 
     dialect: 'mysql',
     port: 3306
   });
 }
-
 
 module.exports = sequelize;
