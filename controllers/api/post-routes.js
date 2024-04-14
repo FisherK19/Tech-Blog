5f12fbde5f36e38331f36b6f250e91cb72bf3217
kristie fisher <fisherkristie19@icloud.com>
2024-04-14 12:39:15 -0400

    fixed duplication in post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -43,41 +43,6 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        include: [
-            {
-                model: User,
-                attributes: ['username']
-            },
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            }
-        ]
-    })
-    .then(dbPostData => {
-        if (!dbPostData) {
-            res.status(404).json({ message: 'No post found with this id' });
-            return;
-        }
-        res.json(dbPostData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
-
-
-// GET a single post by its ID
-router.get('/:id', (req, res) => {
-    Post.findOne({
-        where: {
-            id: req.params.id
-        },
         attributes: ['id', 'title', 'body', 'created_at'],
         include: [
             {
